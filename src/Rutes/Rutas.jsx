fded70a30eaa27a0c25ea1368278153d0de3d34c
Elser Moises <elsermoises@gmail.com>
2025-01-05 12:10:59 -0500

    Modificación de rutas para que pueda soportar Github Pages

--- src/Rutes/Rutas.jsx
+++ src/Rutes/Rutas.jsx
@@ -1,30 +1,25 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
 import Curriculum from "../Pages/Curriculum";
 import Certificados from "../Pages/Certificados";
-import Proyectos from "../Pages/Proyectos";
 import Contacto from "../Pages/Contacto";
 import Layout from "../Componentes/Layout/Layout"; 
 
 export const Rutas = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>     
         <Route path="/" element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="curriculum" element={<Curriculum />} />
-            <Route path="certificados" element={<Certificados />} />
-            <Route path="proyectos" element={<Proyectos />} />
+            <Route path="certificados" element={<Certificados />} />          
             <Route path="contacto" element={<Contacto />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
 export default Rutas;
-
-
-
