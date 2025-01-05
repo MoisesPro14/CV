ff568121e339b02a3c33a2ffb546385405e5afde
Elser Moises <elsermoises@gmail.com>
2025-01-05 13:15:52 -0500

    .

--- src/Componentes/Layout/Layout.jsx
+++ src/Componentes/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import Header from "../Encabezado/Header";
 export const Layout = () => {
   return (
     <> 
-      <div className="min-h-screen flex flex-col bg-gradient-to-br from-slate-600 via-slate-950 to-slate-600 animate-gradient-flow">
+      <div className="min-h-screen flex flex-col bg-gradient-to-br from-slate-700 via-slate-950 to-slate-700 animate-gradient-flow">
         <Header />
         <main className="flex-1 mb-16">
             <Outlet />
