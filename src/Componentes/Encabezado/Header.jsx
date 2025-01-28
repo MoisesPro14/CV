a4503d1f686bdba2a8abb3e50a386bf444b2e6db
Elser Moises <elsermoises@gmail.com>
2025-01-28 14:36:53 -0500

    .

--- src/Componentes/Encabezado/Header.jsx
+++ src/Componentes/Encabezado/Header.jsx
@@ -11,12 +11,12 @@ export const Header = () => {
 
   return (
     <>
-        <div className="flex overflow-x-auto sm:overflow-visible my-7 md:m-8 p-2 scrollbar-hide">
-            <ul className="flex space-x-4  ml-auto">
+        <div className="flex overflow-x-auto sm:overflow-visible mt-7 mb-8 md:m-8 p-2 scrollbar-hide bg-transparent">
+            <ul className="flex space-x-4  ml-auto ">
                 <li>
                     <Link to="/"
-                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium shadow-[0_4px_8px_rgba(0,0,0,0.2)] transform transition-all duration-300
-                        ${isActive('/') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`} >
+                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium  transform transition-all duration-300
+                        ${isActive('/') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 ow-cdark:focus:ring-cyan-800' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`} >
                     <TbHomeInfinity className="text-red-600 text-xl m-1 transform hover:scale-125 group-hover:rotate-[360deg] transition-transform duration-300" />
                     Home
 
@@ -25,8 +25,8 @@ export const Header = () => {
 
                 <li>
                     <Link to="/about"
-                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium shadow-[0_4px_8px_rgba(0,0,0,0.2)] transform transition-all duration-300
-                        ${isActive('/about') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`}>
+                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium  transform transition-all duration-300
+                        ${isActive('/about') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 ' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`}>
                     <BsPersonFill className="text-blue-600 text-xl m-1 transform hover:scale-125 group-hover:rotate-[360deg] transition-transform duration-300" /> 
                         Perfil
                   
@@ -35,8 +35,8 @@ export const Header = () => {
 
                 <li>
                     <Link to="/curriculum"
-                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium shadow-[0_4px_8px_rgba(0,0,0,0.2)] transform transition-all duration-300
-                        ${isActive('/curriculum') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`}>
+                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium  transform transition-all duration-300
+                        ${isActive('/curriculum') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 ' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`}>
                         <BsFillBookmarksFill className="text-gray-800 text-xl m-1 transform hover:scale-125 group-hover:rotate-[360deg] transition-transform duration-300"/>  
                         Currículum 
                        
@@ -45,8 +45,8 @@ export const Header = () => {
 
                 <li>
                     <Link to="/certificados"
-                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium shadow-[0_4px_8px_rgba(0,0,0,0.2)] transform transition-all duration-300
-                        ${isActive('/certificados') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`} >
+                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium  transform transition-all duration-300
+                        ${isActive('/certificados') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 ' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`} >
                         <PiCertificateDuotone className="text-purple-600 text-xl m-1 transform hover:scale-125 group-hover:rotate-[360deg] transition-transform duration-300" /> 
                         Certificados
                        
@@ -55,8 +55,8 @@ export const Header = () => {
 
                 <li>
                     <Link  to="/proyectos"
-                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium shadow-[0_4px_8px_rgba(0,0,0,0.2)] transform transition-all duration-300
-                        ${isActive('/proyectos') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`}>
+                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium  transform transition-all duration-300
+                        ${isActive('/proyectos') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 ' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`}>
                         <SiCountingworkspro className="text-orange-600 text-xl m-1 transform hover:scale-125 group-hover:rotate-[360deg] transition-transform duration-300" />
                         Proyectos
                         
@@ -65,8 +65,8 @@ export const Header = () => {
 
                 <li>
                     <Link to="/contacto"
-                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium shadow-[0_4px_8px_rgba(0,0,0,0.2)] transform transition-all duration-300
-                        ${isActive('/contacto') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`} >
+                    className={`group flex items-center space-x-2 p-2 rounded-lg font-medium transform transition-all duration-300
+                        ${isActive('/contacto') ? 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 ' : 'text-gray-500 bg-white hover:bg-gray-300 hover:scale-105'}`} >
                     <PiTelegramLogoFill className="text-teal-600 text-xl m-1 transform hover:scale-125 group-hover:rotate-[360deg] transition-transform duration-300" /> 
                     Contacto
                     
